5bbd435d39a8addc9ff9adf15d9e2534d7d8f8b2
ronitebul1211 <ronitebul1211@gmail.com>
2020-11-16 15:51:38 +0200

    access theme context from navbar

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -1,5 +1,14 @@
 import React, { createContext, Component } from "react";
 
+/**
+ ThemeContextProvider is wrapper for the consumer components tree.
+
+ the consumer -> navbar declare in his class -> static contextType = ThemeContext;
+ then its look up components tree to the first element hold the provider for these context type -> ThemeContextProvider,
+ its take the value property of the ThemeContext.Provider and attach it to component context (Navbar), 
+ access by this.context
+
+ */
 export const ThemeContext = createContext();
 
 class ThemeContextProvider extends Component {
