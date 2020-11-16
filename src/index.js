bd72dbdf7e821d0f0738d7f17fc84c879d30f8db
ronitebul1211 <ronitebul1211@gmail.com>
2020-11-16 19:17:11 +0200

    extract context app to separate directory

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import ContextApp from "./ContextApp/ContextApp";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<ContextApp />, document.getElementById("root"));
