0189dfd2c3778fd45513c58bb0f3d4e87a0d2b24
ronitebul1211 <ronitebul1211@gmail.com>
2020-11-16 21:26:49 +0200

    create Books context and add components files

--- src/BookListFinal/BookListFinalApp.js
+++ src/BookListFinal/BookListFinalApp.js
@@ -1,7 +1,12 @@
 import React from "react";
+import BookContextProvider from "./contexts/BookContext";
 
 function App() {
-   return <div className="App">Book list final</div>;
+   return (
+      <div className="App">
+         <BookContextProvider></BookContextProvider>
+      </div>
+   );
 }
 
 export default App;
---
+++ src/BookListFinal/contexts/BookContext.js
@@ -0,0 +1,24 @@
+import React, { createContext, useState } from "react";
+import { v1 as uuid } from "uuid";
+
+export const BookContext = createContext();
+
+const BookContextProvider = (props) => {
+   const [books, setBooks] = useState([
+      { id: 1, title: "name of the wind", author: "patrick rothfuss" },
+      { id: 1, title: "the final empire", author: "brandon sanderson" },
+   ]);
+
+   const addBook = (title, author) => {
+      setBooks([...books, { title, author, id: uuid() }]);
+   };
+
+   const removeBook = (id) => {
+      setBooks(books.filter((book) => book.id !== id));
+   };
+   return (
+      <BookContext.Provider value={{ books, addBook, removeBook }}>{props.children}</BookContext.Provider>
+   );
+};
+
+export default BookContextProvider;
