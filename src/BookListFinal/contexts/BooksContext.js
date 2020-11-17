bba2218bfd4b5d178f82040f993d6c8aa3f763c9
ronitebul1211 <ronitebul1211@gmail.com>
2020-11-17 09:29:37 +0200

    save books data in local storage

--- src/BookListFinal/contexts/BooksContext.js
+++ src/BookListFinal/contexts/BooksContext.js
@@ -1,10 +1,24 @@
-import React, { createContext, useReducer } from "react";
+import React, { createContext, useEffect, useReducer } from "react";
 import { BookReducer } from "../reducers/BookReducer";
 
+/**
+ useReducer Hook get 3 argument
+ - reference to reducer function
+ - initial value
+ - default value return from function
+ */
 export const BooksContext = createContext();
 
 const BooksContextProvider = (props) => {
-   const [books, dispatch] = useReducer(BookReducer, []);
+   //Init books from local storage data / empty array (no data)
+   const [books, dispatch] = useReducer(BookReducer, [], () => {
+      const localData = localStorage.getItem("books");
+      return localData ? JSON.parse(localData) : [];
+   });
+   //When books change, save updated copy in local storage
+   useEffect(() => {
+      localStorage.setItem("books", JSON.stringify(books));
+   }, [books]);
 
    return <BooksContext.Provider value={{ books, dispatch }}>{props.children}</BooksContext.Provider>;
 };
